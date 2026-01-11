61dd035e9f41df9107a88234563896bfea250e67
unknown <riditjangra09@gmail.com>
2026-01-11 23:44:30 +0530

    chore: updated cover image

--- components/editor-component.tsx
+++ components/editor-component.tsx
@@ -185,6 +185,7 @@ export function EditorComponent({
                   alt="cover-image"
                   width={350}
                   height={300}
+                  style={{ width: "100%", height: "100%" }}
                 />
                 <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex items-center justify-center">
                   <Button
